54becd4ebda5b385fa12e0945862b47757435b2d
Hanush Patibandla <hancsh.the.1@outlook.com>
2025-09-28 22:49:57 -0700

    I think the payment part is working properly

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'your_secret_key')
 
 
-app.config['MYSQL_HOST'] = '10.0.2.49'
-app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'root')
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = os.environ['DB_NAME']
 
